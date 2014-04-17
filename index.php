34ebfa00457a7cc36757a118ed11d4b1dae1e34a
Richard <r.mahr@omnicom.de>
2014-04-17 10:12:32 +0200

    Grundgerüst

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+  "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=ISO-8859-1">
+<link rel="shortcut icon" type="image/x-icon" href="images/guitar.ico">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Get Your Gig</title>
+<script src="scripts/jquery-2.1.0.js" type="text/javascript"></script> 
+<script src="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js"></script>
+<script src="scripts/functions.js" type="text/javascript"></script> 
+</head>
+<body>
+	<div id="everything" class="fullwidth round">
+		<div id="header" class="innerfull round">
+			header
+		</div>
+		
+		<div id="navigation" class="innerfull round">
+			navigation
+		</div>
+		
+		<div id="content" class="innerfull round">
+			content
+		</div>
+		
+		<div id="footer" class="innerfull round">
+			footer
+		</div>
+		
+	</div>
+</body>
+</html>
